6cd444bc8da3b450131a4a2fa40141605d51c1d4
Susan <susan8098@gmail.com>
2020-04-28 10:55:27 -0400

    Updates

---
+++ precache-manifest.398a9342062b4237fd20a0c2085ee626.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c107e7cabbe091c11708582fea7ffab0",
+    "url": "/ghibli-films/index.html"
+  },
+  {
+    "revision": "74e2c96085ae7c02ee71",
+    "url": "/ghibli-films/static/css/main.57fc983e.chunk.css"
+  },
+  {
+    "revision": "5445b67beb8d38c51885",
+    "url": "/ghibli-films/static/js/2.578967b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ghibli-films/static/js/2.578967b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74e2c96085ae7c02ee71",
+    "url": "/ghibli-films/static/js/main.e4544607.chunk.js"
+  },
+  {
+    "revision": "42cf450325083ca1e192",
+    "url": "/ghibli-films/static/js/runtime-main.c1b3e32e.js"
+  }
+]);
